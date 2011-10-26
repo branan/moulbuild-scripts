2b1915eaa59adb8abdeb5f2960b6452b82177855
Branan Purvine-Riley <branan@gmail.com>
2011-10-25 18:28:07 -0700

    Don't specify targets for jom, to avoid re-checking a million dependencies

--- build_cwe.bat
+++ build_cwe.bat
@@ -2,7 +2,7 @@ call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
 ECHO ON
 
 cd C:\urulive\build\Plasma_int
-jom -j4 plClient MaxMain MaxPlasmaLights plPythonPack
+jom -j4
 IF ERRORLEVEL 1 GOTO BUILDFAIL
 copy /y bin\plClient.exe ..\..\staging\plClient.exe
 copy /y bin\PlasmaMax.gup ..\..\staging\plugins\PlasmaMax.gup
@@ -10,7 +10,7 @@ copy /y bin\MaxPlasmaLights.dlo ..\..\staging\plugins\MaxPlasmaLights.dlo
 copy /y bin\plPythonPack.exe ..\..\prefix\bin\plPythonPack.exe
 
 cd C:\urulive\build\Plasma_ext
-jom -j4 plClient plUruLauncher
+jom -j4
 IF ERRORLEVEL 1 GOTO BUILDFAIL
 copy /y bin\UruExplorer.exe ..\..\staging\UruExplorer.exe
 copy /y bin\UruLauncher.exe ..\..\staging\UruLauncher.exe
